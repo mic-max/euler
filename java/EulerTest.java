efbe1ab2051d099a8f6a720f097b6613e206fefe
Michael Maxwell <mike.ian.maxwell@gmail.com>
2018-12-21 16:41:14 -0500

    problem 1

---
+++ java/EulerTest.java
@@ -0,0 +1,7 @@
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public final class EulerTest {
+
+	@Test public void testP001() { assertEquals("233168", new p001().run()); }
+}
---
+++ java/p001.java
@@ -0,0 +1,13 @@
+public final class p001 implements EulerSolution {
+
+	public String run() {
+		int sum = 0;
+
+		for (int i = 1; i < 1000; i++) {
+			if (i % 3 == 0 || i % 5 == 0)
+				sum += i;
+		}
+
+		return String.format("%d", sum);
+	}
+}
